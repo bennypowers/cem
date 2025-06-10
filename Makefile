367047a025698d28d75d521bcea9f745e17d1c97
Benny Powers <web@bennypowers.com>
2025-06-10 08:05:04 +0300

    test: benchmarks

--- Makefile
+++ Makefile
@@ -7,3 +7,17 @@ update:
 # watch:
 # 	-while true; do ls ./**/*.{go,scm,ts} | entr -dcr sh -c "make test"; done
 
+bench:
+	go test -cpuprofile=cpu.out -bench=BenchmarkGenerate ./generate/...
+
+# Benchmarks	go test -bench=. ./...	Find slow tests
+
+# Capture CPU hotspots
+profile:
+	go test -bench=... -cpuprofile=cpu.out
+
+flamegraph:
+	go tool pprof -http=:8080 cpu.out # Visual analysis
+
+coverage:
+	go test -coverprofile=cover.out
---
+++ generate/generate_benchmark_test.go
@@ -0,0 +1,28 @@
+package generate
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+// BenchmarkGenerate runs the Generate function on all test fixtures to measure performance.
+func BenchmarkGenerate(b *testing.B) {
+	// Gather all .ts files in the test-fixtures directory as input.
+	matches, err := filepath.Glob("test-fixtures/*.ts")
+	if err != nil {
+		b.Fatal(err)
+	}
+	if len(matches) == 0 {
+		b.Skip("No test fixtures found")
+	}
+
+	excludes := []string{}
+
+	for b.Loop() {
+		// Run the Generate function, measuring its speed.
+		err, _ := Generate(matches, excludes)
+		if err != nil {
+			b.Errorf("Generate returned error: %v", err)
+		}
+	}
+}
