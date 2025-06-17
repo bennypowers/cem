2b1789a5c564e1bd3db1ece78e49dc055a2b2dc1
Benny Powers <web@bennypowers.com>
2025-06-17 12:53:12 +0200

    chore: cd

---
+++ npm/install-platform-binary.js
@@ -0,0 +1,23 @@
+import { platform, arch } from "node:process";
+import { execSync } from "node:child_process";
+
+const pkg = {
+  "darwin-x64": "@pwrs/cem-darwin-x64",
+  "darwin-arm64": "@pwrs/cem-darwin-arm64",
+  "linux-x64": "@pwrs/cem-linux-x64",
+  "linux-arm64": "@pwrs/cem-linux-arm64"
+}[`${platform}-${arch}`];
+
+if (!pkg) {
+  console.error(
+    `Unsupported platform: ${platform}-${arch}. Please check https://github.com/bennypowers/cem for supported platforms.`
+  );
+  process.exit(1);
+}
+
+try {
+  execSync(`npm install --no-save ${pkg}`, { stdio: "inherit" });
+} catch (err) {
+  console.error(`Failed to install platform binary package: ${pkg}`);
+  process.exit(1);
+}
