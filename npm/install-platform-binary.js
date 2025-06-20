4c05f2a33f5f10f8191bc2ad2f7ba6fee5008f49
Benny Powers <web@bennypowers.com>
2025-06-20 14:36:07 +0200

    fix: syntax error in postinstall script

--- npm/install-platform-binary.js
+++ npm/install-platform-binary.js
@@ -5,9 +5,9 @@ const pkg = {
   "darwin-x64": "@pwrs/cem-darwin-x64",
   "darwin-arm64": "@pwrs/cem-darwin-arm64",
   "linux-x64": "@pwrs/cem-linux-x64",
-  "linux-arm64": "@pwrs/cem-linux-arm64"
+  "linux-arm64": "@pwrs/cem-linux-arm64",
   "win32-x64": "@pwrs/cem-win32-x64",
-  "win32-arm64": "@pwrs/cem-win32-arm64"
+  "win32-arm64": "@pwrs/cem-win32-arm64",
 }[`${platform}-${arch}`];
 
 if (!pkg) {
