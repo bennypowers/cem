ccdc7f2bae61c22013dce5f53a7249ac556a01e1
Benny Powers <web@bennypowers.com>
2025-06-26 16:17:06 +0300

    test: demo jsdoc tag

---
+++ generate/test-fixtures/jsdoc-demo.ts
@@ -0,0 +1,7 @@
+/**
+ * @demo https://example.com/jsdoc-alias
+ * @demo https://example.com/jsdoc-alias-description - markdown description
+ */
+@customElement('jsdoc-demo')
+class JsdocDemo extends LitElement {
+}
