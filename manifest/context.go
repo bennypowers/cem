3af050b5b7729a3c3cc87b5d9d138cdadc5eb976
Benny Powers <web@bennypowers.com>
2025-07-03 18:06:38 +0300

    feat(list): list modules
    Closes #30

--- manifest/context.go
+++ manifest/context.go
@@ -20,6 +20,7 @@ import (
 	"errors"
 	"slices"
 	"strconv"
+	"strings"
 )
 
 func (x *Package) GetAllTagNames() (tags []string) {
@@ -38,6 +39,22 @@ func (x *Package) GetAllTagNames() (tags []string) {
 
 type RenderableMemberWithContext interface {
 	ToTableRow() []string
+}
+
+type ModuleWithContext struct {
+	Path                     string
+	Module                   *Module
+	CustomElementExports     []CustomElementExport
+}
+func (x ModuleWithContext) ToTableRow() []string {
+	tags := make([]string, 0)
+	for _, cee := range x.CustomElementExports {
+		tags = append(tags, cee.Name)
+	}
+	return []string{
+		x.Path,
+		strings.Join(tags, ", "),
+	}
 }
 
 type CustomElementWithContext struct {
@@ -220,6 +237,28 @@ func (m MethodWithContext) ToTableRow() []string {
 		strconv.FormatBool(m.Method.Static),
 		m.Method.Summary,
 	}
+}
+
+// GetAllTModulesWithContext returns a slice of JavascriptModuleWithContext for all modules.
+func (x *Package) GetAllModulesWithContext() (modules []ModuleWithContext) {
+	ms := make(map[string]ModuleWithContext)
+	for _, m := range x.Modules {
+		ms[m.Path] = ModuleWithContext{Path: m.Path, Module: &m}
+		for _, e := range m.Exports {
+			if cee, ok := e.(*CustomElementExport); ok {
+				c := ms[m.Path]
+				c.CustomElementExports = append(ms[m.Path].CustomElementExports, *cee)
+				ms[m.Path] = c
+			}
+		}
+	}
+	for _, m := range ms {
+		modules = append(modules, m)
+	}
+	slices.SortStableFunc(modules, func(a ModuleWithContext, b ModuleWithContext) int {
+		return strings.Compare(a.Path, b.Path)
+	})
+	return modules
 }
 
 // GetAllTagNamesWithContext returns a slice of CustomElementWithContext for all custom elements in all modules.
