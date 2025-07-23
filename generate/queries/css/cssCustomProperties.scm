abd626dbaee433f1a8a31fb0a7c32fb7cf31d5c8
Benny Powers <web@bennypowers.com>
2025-07-23 21:07:00 +0300

    fix(generate): comment for prop when the val is a var call

--- generate/queries/css/cssCustomProperties.scm
+++ generate/queries/css/cssCustomProperties.scm
@@ -1,3 +1,37 @@
+( ; /** defined on host */
+  ; --var: blue
+  (rule_set
+    (selectors
+      (pseudo_class_selector
+        (class_name) @class.name (#eq? @class.name "host")))
+    (block (
+      (comment)? @comment (#match? @comment "^/\\*\\*")
+      .
+      (declaration
+        (property_name) @property (#match? @property "^--[^_]")
+        ":"
+        (_)* @default)) @cssProperty)))
+
+( ; /** single var call in declaration */
+  ; color: var(--blue);
+  (rule_set
+    (selectors) @selectors (#not-match? @selectors ":host")
+    (block (
+      (comment)? @comment (#match? @comment "^/\\*\\*")
+      .
+      (declaration
+        (property_name) @prop.name (#not-match? @prop.name "^--[^_]")
+        ":"
+        (call_expression
+          (function_name) @fn (#eq? @fn "var")
+          (arguments
+            "("
+            .
+            (plain_value) @property (#match? @property "^--[^_]")
+            ("," [_(_)]* @default)?
+            .
+            ")")))) @cssProperty)))
+
 ( ; color: /** blue */ var(--blue);
   ; color: light-dark(/** blue */
   ;                   var(--blue),
@@ -13,37 +47,5 @@
       (plain_value) @property (#match? @property "^--[^_]")
       ("," [_(_)]* @default)?
       .
-      ")"))) @cssProperty
+      ")"))) @cssProperty;
 
-( ; /** defined on host */
-  ; --var: blue
-  (rule_set
-    (selectors
-      (pseudo_class_selector
-        (class_name) @class.name (#eq? @class.name "host")))
-    (block
-      (comment)? @comment (#match? @comment "^/\\*\\*")
-      .
-      (declaration
-        (property_name) @property (#match? @property "^--[^_]")
-        ":"
-        (_)* @default) @cssProperty)))
-
-( ; /** single var call in declaration */
-  ; color: var(--blue);
-  (rule_set
-    (block
-      (comment)? @comment (#match? @comment "^/\\*\\*")
-      .
-      (declaration
-        (property_name)
-        ":"
-        (call_expression
-          (function_name) @fn (#eq? @fn "var")
-          (arguments
-            "("
-            .
-            (plain_value) @property (#match? @property "^--[^_]")
-            ("," [_(_)]* @default)?
-            .
-            ")"))) @cssProperty)))
