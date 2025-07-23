ec6942172398ac02da8b446d0d0a20313172a722
Benny Powers <web@bennypowers.com>
2025-07-23 17:35:22 +0300

    feat(generate): allow css var comments above declarations
    e.g.
    ```css
    /** blue hue */
    color: var(--blue);
    ```

---
+++ generate/test/fixtures/project-css/src/class-css-described-declaration.ts
@@ -0,0 +1,5 @@
+import styles from './class-css-described-declaration.css'
+@customElement('class-css-described-declaration')
+class ClassCssDescribedDeclaration extends LitElement {
+  static styles = styles;
+}
