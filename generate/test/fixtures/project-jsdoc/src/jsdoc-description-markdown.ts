8654403d2a4d67a553349c10747b005d0242e02e
Benny Powers - עם ישראל חי! <bennypowers@users.noreply.github.com>
2025-10-16 10:26:37 +0300

    fix(generate): parsing jsdoc descriptions (#119)
    * test(generate): case for proper parsing of markdown in jsdoc
    
    * fix(generate): parsing jsdoc descriptions
    
    removes preceeding asterisks from description lines n>1
    see https://github.com/tree-sitter/tree-sitter-jsdoc/issues/28

---
+++ generate/test/fixtures/project-jsdoc/src/jsdoc-description-markdown.ts
@@ -0,0 +1,14 @@
+/**
+ * description
+ *
+ * the description contains:
+ * - markdown list
+ * - inline code
+ * - multiple lines
+ * - markdown headings
+ *
+ * ## Heading
+ * markdown heading follows `list`.
+ */
+@customElement('jsdoc-description-markdown')
+class JsdocDescriptionMarkdown extends LitElement { }
